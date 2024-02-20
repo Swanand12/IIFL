67df287716aaa5f90ef4a80320723ff908f52d62
Kanhaiya <mkanhaiyas149@gmail.com>
2024-02-20 12:14:33 +0530

    Porject Structure made

---
+++ src/pages/Home.js
@@ -0,0 +1,14 @@
+import Navbar from "../components/Navbar";
+import Footer from "../components/Footer";
+
+function Home() {
+  return (
+    <div>
+      <Navbar />
+      Home
+      <Footer />
+    </div>
+  );
+}
+
+export default Home;
