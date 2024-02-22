0689ff08dab3ee3fb5dc702cbae10dbd5e2205df
Kanhaiya <mkanhaiyas149@gmail.com>
2024-02-22 23:06:08 +0530

    tailwind added

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,5 +1,45 @@
 function Navbar() {
-  return <div>Navbar</div>;
+  return (
+    <div id="navbar" className="flex ">
+      <div id="nav_logo">
+        <img src="../../Images/logo.png" alt="logo.png" />
+      </div>
+      <div id="nav_menu">
+        <li>
+          <a href="a">HOME</a>
+        </li>
+        <li>
+          <a href="a">ABOUT US</a>
+          <div className="nav_menu_dropdown">
+            <li className="dropmenu">
+              <a href="a">Overview</a>
+            </li>
+            <li>
+              <a href="a">Why Waterfront?</a>
+            </li>
+            <li>
+              <a href="a">Our Team</a>
+            </li>
+          </div>
+        </li>
+        <li>
+          <a href="a">INVESTMENTS</a>
+        </li>
+        <li>
+          <a href="a">LOANS</a>
+        </li>
+        <li>
+          <a href="a">WORK WITH US</a>
+        </li>
+        <li>
+          <a href="a">REACH US</a>
+        </li>
+        <li>
+          <a href="a">CLIENT LOGIN</a>
+        </li>
+      </div>
+    </div>
+  );
 }
 
 export default Navbar;
