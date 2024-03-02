b2cf3c3f5aa66ce98302a0198358f868a3bcd8f2
Kanhaiya <mkanhaiyas149@gmail.com>
2024-03-02 17:21:42 +0530

    comp

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -29,7 +29,21 @@ function Navbar() {
           </div>
           <span className="flex items-center">
             <MenuIcon
-              onClick={() => setNavBar((arr) => [...arr, (arr[0] = !arr[0])])}
+              onClick={() =>
+                setNavBar((arr) => [
+                  !arr[0],
+                  arr[1],
+                  arr[2],
+                  arr[3],
+                  arr[4],
+                  arr[5],
+                  arr[6],
+                  arr[7],
+                  arr[8],
+                  arr[9],
+                  arr[10],
+                ])
+              }
             />
           </span>
         </div>
